e8c9d1ddf99948f1123b58a5efa03d646825eb13
harshithav8453 <harshithav8453@gmail.com>
2026-04-18 10:14:09 +0530

    Add files via upload

---
+++ firstprogram.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class firstprogram {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Everyone,WELCOME TO AJP COURSE");
+		
+	}
+
+}
---
+++ secondprogram.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class secondprogram {
+	public static void main(String[] args) {
+		int age=20;
+		float height=4.5f;
+		System.out.println("the age od the student"+age);
+		System.out.println("the height of the student"+height);
+		
+	}
+
+}
